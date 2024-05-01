d720ecd364075549739580d4d4a10dd11f6d25d4
Fressia <fressiabarrios2@gmail.com>
2024-05-01 23:21:58 +0200

    included autofix

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import TEST_ID from "./Nav.testid";
-
 const Nav = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // starts at false because we assume user is not logged in ** to be replaced with real data once we manage sessions **
+
   return (
     <ul>
-      <Link to="/" data-testid={TEST_ID.linkToHome}>
-        <li>Home</li>
+      <Link to="/">
+        <li>Logo</li>
       </Link>
-      <Link to="/user" data-testid={TEST_ID.linkToUsers}>
-        <li>Users</li>
-      </Link>
+      {/* Check if user is logged in to show different options than we show to guests */}
+      {!isLoggedIn ? 
+      <>
+        <Link to="/signin">
+          <li>Sign in</li>
+        </Link>
+        <Link to="/signup">
+          <li>Sign up</li>
+        </Link>
+      </> : 
+      <>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+        <Link to="/profile">
+          <li>Profile</li>
+        </Link>
+        <Link to="/logout">
+          <li>Log out</li>
+        </Link>
+      </> }
     </ul>
   );
 };
---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+const Nav = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // starts at false because we assume user is not logged in ** to be replaced with real data once we manage sessions **
+
+  const handleSignIn = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogOut = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <nav className="nav-bar-container">
+      <Link to="/">
+        <p>Logo</p>
+      </Link>
+      {/* Check if user is logged in to show different options than we show to guests */}
+      {!isLoggedIn ? (
+        <ul className="nav-menu">
+          <Link to="/">
+            <button onClick={handleSignIn} className="sign-in-btn">
+              Sign in
+            </button>
+          </Link>
+          <Link to="/signup">
+            <button className="sign-up-btn">Sign up</button>
+          </Link>
+        </ul>
+      ) : (
+        <ul className="nav-menu">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/profile">
+            <li>Profile</li>
+          </Link>
+          <Link to="/">
+            <button onClick={handleLogOut} className="logout-btn">
+              Log out
+            </button>
+          </Link>
+        </ul>
+      )}
+    </nav>
+  );
+};
+
+export default Nav;
