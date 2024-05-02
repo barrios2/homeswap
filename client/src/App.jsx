689e3341754f7bab8c74b7fc44d54cbfc82bd556
Fressia <fressiabarrios2@gmail.com>
2024-05-02 14:57:34 +0200

    included routes based on menu item routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav";
 import Home from "./pages/Home/Home";
 import CreateUser from "./pages/User/CreateUser";
 import UserList from "./pages/User/UserList";
+import AboutUs from "./pages/AboutUs/AboutUs";
 
 const App = () => {
   return (
     <>
-      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route
+          path="/profile"
+          element={<p>to be added when profile page is ready</p>}
+        />
         <Route path="/user" element={<UserList />} />
         <Route path="/user/create" element={<CreateUser />} />
       </Routes>
