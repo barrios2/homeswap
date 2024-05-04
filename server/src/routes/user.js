19df267444dd5a1e6bb901feb9c2f65d7c376b6f
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-05-04 20:05:49 +0200

    fixed unused vars

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { /*createUser*/ getUsers, signUp } from "../controllers/user.js";
+import {
+  /*createUser, getUsers,*/ signUp,
+  signIn,
+} from "../controllers/user.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/", getUsers);
+//userRouter.get("/", getUsers);
 userRouter.post("/create", signUp);
+userRouter.post("/login", signIn);
 
 export default userRouter;
