a4e58936d9c4410be868990c3312cfddcecc0474
Fressia <fressiabarrios2@gmail.com>
2024-05-04 13:28:01 +0200

    removed duplicate classes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LogIn from "./components/LogIn/LogIn";
 import SignUp from "./components/SignUp/SignUp";
-import Home from "./pages/Home/Home";
+// import Home from "./pages/Home/Home";
 import AboutUs from "./pages/AboutUs/AboutUs";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        {/* <Route path="/home" element={<Home />} /> */}
         <Route path="/" element={<AboutUs />} />
         <Route path="/user/signup" element={<SignUp />} />
         <Route path="/user/login" element={<LogIn />} />
