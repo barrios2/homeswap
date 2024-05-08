00641e79c12ce63f1c37a719f4dabdfffab369c8
Fressia <fressiabarrios2@gmail.com>
2024-05-08 17:23:47 +0200

    deleted unnecessary styling classes

--- client/src/components/AboutUsContent/AboutUsContent.jsx
+++ client/src/components/AboutUsContent/AboutUsContent.jsx
@@ -15,7 +15,7 @@ const AboutUsContent = () => {
             <h1 className="belief-header">
               We believe in a more responsible way to go on holidays
             </h1>
-            <p>
+            <p className="belief-text">
               Travel should not happen at the expense of the local community, it
               should be about connecting with people, & sharing is the most
               responsible way to use resources.
