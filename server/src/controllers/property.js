17c82fe3dfc14c609d5d41c87532c1f71de9dfd9
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-05-08 13:42:10 +0200

    search-backend

--- server/src/controllers/property.js
+++ server/src/controllers/property.js
@@ -24,3 +24,42 @@ export const uploadProperty = async (req, res) => {
     });
   }
 };
+
+export const searchProperties = async (req, res) => {
+  try {
+    //for pagination:
+    const limit = parseInt(req.query.limit, 10) || 6;
+    const page = parseInt(req.query.page, 10) || 1;
+    const offset = (page - 1) * limit;
+
+    const { country, city, type, bedrooms, amenities /*availability*/ } =
+      req.query;
+    let filter = {};
+
+    if (country)
+      filter["address.country"] = { $regex: new RegExp(`^${country}$`, "i") };
+    if (city) filter["address.city"] = { $regex: new RegExp(`^${city}$`, "i") };
+    if (type) filter.type = { $regex: new RegExp(`^${type}$`, "i") };
+    if (bedrooms) filter.bedrooms = { $gte: parseInt(bedrooms, 10) };
+    if (amenities) filter.amenities = { $in: amenities };
+
+    const properties = await Property.find(filter).limit(limit).skip(offset);
+
+    const total = await Property.countDocuments(filter);
+
+    res.status(200).json({
+      success: true,
+      data: properties,
+      total,
+      page,
+      lastPage: Math.ceil(total / limit),
+      filterApplied: filter,
+    });
+  } catch (error) {
+    logError(error);
+    res.status(500).json({
+      success: false,
+      msg: "Unable to get properties, try again later",
+    });
+  }
+};
--- server/src/routes/property.js
+++ server/src/routes/property.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { uploadProperty } from "../controllers/property.js";
+import { uploadProperty, searchProperties } from "../controllers/property.js";
 import { verifyUser } from "../util/verifyUser.js";
 
 const propertyRouter = express.Router();
 
 propertyRouter.post("/upload", verifyUser, uploadProperty);
+propertyRouter.get("/search", searchProperties);
 
 export default propertyRouter;
