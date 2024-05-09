5f6a0bd209e1e342d27e38e309f69bba6e2256d7
Fressia <fressiabarrios2@gmail.com>
2024-05-09 18:40:55 +0200

    Included nav, property lis and footer component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-
-import TEST_ID from "./Home.testid";
+import Nav from "../../components/Nav/Nav";
+import PropertyList from "../../components/PropertyList/PropertyList";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
-    <div data-testid={TEST_ID.container} style={{ color: "yellow" }}>
-      <h1>This is the homepage</h1>
-      <p>Good luck with the project!</p>
-    </div>
+    <>
+      <Nav />
+      <hr />
+      <PropertyList />
+      <hr />
+      <Footer />
+    </>
   );
 };
 
