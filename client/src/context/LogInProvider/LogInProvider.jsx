3000197550513c4125869c6ad6a1f8c714059bf9
Fressia <fressiabarrios2@gmail.com>
2024-05-11 17:51:29 +0200

    included uploadProperty 1st screen form details in context

--- client/src/context/LogInProvider/LogInProvider.jsx
+++ client/src/context/LogInProvider/LogInProvider.jsx
@@ -8,9 +8,38 @@ const LogInContext = createContext({
 
 const LogInProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // pre define form data fields
+  const [formData, setFormData] = useState({
+    title: "",
+    country: "",
+    city: "",
+    street: "",
+    houseNumber: "",
+    postcode: "",
+    homeType: "",
+  });
+  // pre define form fields for which we will need errors
+  const [formErrors, setFormErrors] = useState({
+    title: "",
+    country: "",
+    city: "",
+    street: "",
+    houseNumber: "",
+    postcode: "",
+    homeType: "",
+  });
 
   return (
-    <LogInContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+    <LogInContext.Provider
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        formData,
+        setFormData,
+        formErrors,
+        setFormErrors,
+      }}
+    >
       {children}
     </LogInContext.Provider>
   );
