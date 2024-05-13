76668fbe1e113bfe62ed78b3bffb5818f1c07983
Fressia <fressiabarrios2@gmail.com>
2024-05-13 17:43:28 +0200

    combined all screens

--- client/src/pages/UploadProperty/UploadProperty.jsx
+++ client/src/pages/UploadProperty/UploadProperty.jsx
@@ -1,19 +1,37 @@
 import React, { useEffect } from "react";
 import UploadProperty1 from "../../components/UploadProperty/UploadProperty1/UploadProperty1";
 import UploadProperty2 from "../../components/UploadProperty/UploadProperty2/UploadProperty2";
-// import UploadProperty3 from "../../components/UploadProperty/UploadProperty3/UploadProperty3";
+import UploadProperty3 from "../../components/UploadProperty/UploadProperty3/UploadProperty3";
 import { useLogin } from "../../context/LogInProvider/LogInProvider";
 import UploadPropertyNav from "../../components/UploadProperty/UploadPropertyNav/UploadPropertyNav";
 
 function UploadProperty() {
-  const { firstScreenIsComplete } = useLogin();
+  const {
+    firstScreenIsComplete,
+    secondScreenIsComplete,
+    thirdScreenIsComplete,
+  } = useLogin();
 
-  useEffect(() => {}, [firstScreenIsComplete]);
+  useEffect(() => {}, [
+    firstScreenIsComplete,
+    secondScreenIsComplete,
+    thirdScreenIsComplete,
+  ]);
 
   return (
     <>
       <UploadPropertyNav />
-      {!firstScreenIsComplete ? <UploadProperty1 /> : <UploadProperty2 />}
+      {!firstScreenIsComplete ? (
+        <UploadProperty1 />
+      ) : firstScreenIsComplete && !secondScreenIsComplete ? (
+        <UploadProperty2 />
+      ) : firstScreenIsComplete &&
+        secondScreenIsComplete &&
+        !thirdScreenIsComplete ? (
+        <UploadProperty3 />
+      ) : (
+        <p className="first-screen-container">Fourth screen goes here</p>
+      )}
     </>
   );
 }
