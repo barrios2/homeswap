418056f8efeac3cd44cde5e3b5c2a9a691aa2656
Mariam Isaeva <mariam.hammad8@gmail.com>
2024-05-22 18:56:39 +0200

    autofix - removed comments

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -73,15 +73,13 @@ export const login = async (req, res) => {
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
 
-    res /*.cookie("access_token", token)*/
-      .status(200)
-      .json({
-        success: true,
-        id: validUser._id,
-        username: validUser._doc.username,
-        email: validUser._doc.email,
-        token: token,
-      });
+    res.status(200).json({
+      success: true,
+      id: validUser._id,
+      username: validUser._doc.username,
+      email: validUser._doc.email,
+      token: token,
+    });
   } catch (error) {
     logError(error);
     res
