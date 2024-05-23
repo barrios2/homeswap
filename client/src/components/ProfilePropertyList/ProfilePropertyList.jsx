dcb257587e50a866b04edb65d0648dcc7cf93f81
Fressia <fressiabarrios2@gmail.com>
2024-05-23 21:23:34 +0200

    new property card component + styling (for profile page)

---
+++ client/src/components/ProfilePropertyList/ProfilePropertyList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import MyPropertyCard from "../MyPropertyCard/MyPropertyCard";
+import { useLogin } from "../../context/LogInProvider/LogInProvider";
+
+const ProfilePropertyList = () => {
+  const { userProperties } = useLogin();
+
+  const renderPropertyCards = () => {
+    return userProperties?.map(property => (
+      <MyPropertyCard key={property._id} property={property}/>
+    ));
+  };
+
+  return (
+    <div className="my-property-list-container">
+      {renderPropertyCards()}
+    </div>
+  );
+};
+
+export default ProfilePropertyList;
