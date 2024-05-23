17b12ce8755364ae64264d5509aa11faa1d22616
Fressia <fressiabarrios2@gmail.com>
2024-05-23 21:26:44 +0200

    fetching real user properties data from backend

--- client/src/components/Profile/ProfileComponent.jsx
+++ client/src/components/Profile/ProfileComponent.jsx
@@ -1,34 +1,76 @@
-import React from "react";
-import profileNavigateBackIcon from "../../assets/icon-navigate-back.svg";
+import React, { useEffect, useState } from "react";
 import addIcon from "../../assets/icon-add.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import Nav from "../Nav/Nav";
 import "./ProfileComponent.css";
 import { Link } from "react-router-dom";
+import ProfilePropertyList from "../ProfilePropertyList/ProfilePropertyList";
+import { useLogin } from "../../context/LogInProvider/LogInProvider";
+import useFetch from "../../hooks/useFetch";
 
 function ProfileComponent() {
+  const { userId, token, setUserProperties, username } = useLogin();
+
+  const onReceived = (data) => {
+    setUserProperties(data.data);
+  };
+
+  const { performFetch } = useFetch(`/user/properties/${userId}`, onReceived);
+  const [activeTab, setActiveTab] = useState("My properties");
+  const myProperties = activeTab === "My properties";
+  const myRequests = activeTab === "My requests";
+
+  useEffect(() => {
+    // including bearer token from session bc otherwise there will be an unauthorized response from the endpoint
+    performFetch({ headers: { Authorization: `Bearer ${token}` } });
+  }, []);
+
+  // change activeTab state to the text content of button clicked so that active className can be applied (default is "My properties')
+  const handleClick = (e) => {
+    setActiveTab(e.target.textContent);
+  };
+
   return (
-    <div className="add-profile-page">
-      <div className="profile-header1">
-        <img
-          src={profileNavigateBackIcon}
-          alt="navigate back button"
-          className="profile-navigate-back-icon"
-        />
-        <button className="my-requests-btn-profile">Requests</button>
-        <button className="my-property-btn-profile">My Property</button>
-      </div>
-      <div className="profile-hero-wrapper">
-        <div className="profile-hero">
-          <h1 className="profile-hero-title">
-            Find a home that suits your needs
-          </h1>
+    <>
+      <Nav />
+      <div className="add-profile-page">
+        <button className="add-to-profile">
+          <Link to={"/property/upload"}>
+            <img src={addIcon} alt="add button" />
+          </Link>
+        </button>
+        <div className="card-and-list-container">
+          <div className="card-container">
+            <div className="upper-container">
+              <div className="image-container">
+                <FontAwesomeIcon icon={faUser} className="user-pic-icon" />
+              </div>
+            </div>
+            <div className="lower-container">
+              <div>
+                <h4>{username}</h4>
+              </div>
+              <div className="tab-btns-container">
+                <button
+                  onClick={handleClick}
+                  className={`${myProperties ? "active-tab-profile" : ""} profile-tabs-btn`}
+                >
+                  My properties
+                </button>
+                <button
+                  onClick={handleClick}
+                  className={`${myRequests ? "active-tab-profile" : ""} profile-tabs-btn`}
+                >
+                  My requests
+                </button>
+              </div>
+            </div>
+          </div>
+          <ProfilePropertyList />
         </div>
       </div>
-      <button className="add-to-profile">
-        <Link to={"/property/upload"}>
-          <img src={addIcon} alt="add button" />
-        </Link>
-      </button>
-    </div>
+    </>
   );
 }
 export default ProfileComponent;
